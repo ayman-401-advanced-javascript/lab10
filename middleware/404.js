23e59ab34f27a48393ee1789495c4352551b292d
aymankhawaldeh <57315054+aymankhawaldeh@users.noreply.github.com>
2020-02-08 13:07:50 +0200

    Update 404.js

--- middleware/404.js
+++ middleware/404.js
@@ -1,5 +1,16 @@
 /* eslint-disable no-unused-vars */
 'use strict';
+/**
+ * 404 Middleware
+ * @module middleware/404
+ */
+
+/**
+ * Sends a JSON Formatted 404 Response
+ * @param req {object} Express Request Object
+ * @param res {object} Express Response Object
+ * @param next {function} Express middleware next()
+ */
 
 module.exports = (req,res,next) => {
   let error = { error: 'Resource Not Found' };
